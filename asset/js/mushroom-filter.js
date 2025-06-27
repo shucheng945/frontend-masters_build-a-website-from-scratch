49e7ce5078cbd6d91f87430a3ab14d9624563746
shucheng945 <shucheng945@gmail.com>
2025-06-27 13:54:42 +0800

    Filtering Cards

--- asset/js/mushroom-filter.js
+++ asset/js/mushroom-filter.js
@@ -14,5 +14,24 @@ function updateFilter(e) {
   const filterType = e.target.name;
   currentFilters[filterType] = e.target.value;
 
-  console.log(currentFilters);
+  filterCards();
+}
+
+function filterCards() {
+  cards.forEach((card) => {
+    const season = card.querySelector('[data-season]').dataset.season;
+    const edible = card.querySelector('[data-edible]').dataset.edible;
+
+    const matchesSeason = currentFilters.season === season;
+    const matchesEdible = currentFilters.edible === edible;
+
+    if (
+      (matchesEdible || currentFilters.edible === 'all') &&
+      (matchesSeason || currentFilters.season === 'all')
+    ) {
+      card.hidden = false;
+    } else {
+      card.hidden = true;
+    }
+  });
 }
