db3ce18b8b55f84e21c7948fd24a91962a4a33f2
shucheng945 <shucheng945@gmail.com>
2025-06-29 14:13:35 +0800

    Document View Transitions

--- asset/js/mushroom-filter.js
+++ asset/js/mushroom-filter.js
@@ -8,6 +8,11 @@ const currentFilters = {
   edible: 'all'
 };
 
+cards.forEach((card, index) => {
+  const mushroomId = `mushroom-${index + 1}`;
+  card.style.viewTransitionName = `card-${mushroomId}`;
+});
+
 seasonalFilter.addEventListener('change', updateFilter);
 edibleFilter.addEventListener('change', updateFilter);
 
@@ -15,7 +20,11 @@ function updateFilter(e) {
   const filterType = e.target.name;
   currentFilters[filterType] = e.target.value;
 
-  filterCards();
+  if (!document.startViewTransition()) {
+    filterCards();
+    return;
+  }
+  document.startViewTransition(() => filterCards());
 }
 
 function filterCards() {
